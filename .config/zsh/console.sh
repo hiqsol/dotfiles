187488b6519479a6ab9bd2c2d39b1460c4b2218d
Andrii Vasyliev <sol@hiqdev.com>
2022-09-28 12:12:24 +0300

    install latest neovim instead of stable by default

--- .config/zsh/console.sh
+++ .config/zsh/console.sh
@@ -73,7 +73,8 @@ nvim() {
     file="$HOME/sbin/nvim"
 
     if [ ! -x $file ]; then
-        curl -L https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -o $file
+        #curl -L https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -o $file
+        curl -L https://github.com/neovim/neovim/releases/download/latest/nvim.appimage -o $file
         chmod a+x $file
     fi
 
