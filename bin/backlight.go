82d6870f8a29a95dc80d978ad96a9633421f4ba1
Andrii Vasyliev <sol@hiqdev.com>
2023-07-12 13:53:44 +0300

    add constants to `backlight`

--- bin/backlight.go
+++ bin/backlight.go
@@ -10,6 +10,10 @@ func usage() {
     os.Exit(1)
 }
 
+const MIN_BRIGHTNESS = 1
+const MAX_BRIGHTNESS = 96000
+const CUR_BRIGHTNESS_PATH = "/sys/class/backlight/intel_backlight/brightness"
+
 func main() {
     if len(os.Args) < 2 {
         usage()
@@ -29,16 +33,20 @@ func main() {
     koef := 1.0 + sign * (value / 100)
     //fmt.Println(cmd, sign, value, koef)
 
-    path := "/sys/class/backlight/intel_backlight/brightness"
-    bytes, _ := os.ReadFile(path)
+    bytes, _ := os.ReadFile(CUR_BRIGHTNESS_PATH)
     text := strings.TrimSpace(string(bytes))
     curr, _ := strconv.Atoi(text)
     next := int(float64(curr) * koef)
+    if next < MIN_BRIGHTNESS {
+        next = MIN_BRIGHTNESS
+    } else if next > MAX_BRIGHTNESS {
+        next = MAX_BRIGHTNESS
+    }
     //fmt.Println(text, curr, next)
 
     new_text := strconv.Itoa(next) + "\n"
 
-    err := os.WriteFile(path, []byte(new_text), 0644)
+    err := os.WriteFile(CUR_BRIGHTNESS_PATH, []byte(new_text), 0644)
     if err != nil {
         fmt.Println(err)
     }
