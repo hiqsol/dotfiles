dea28f16f0fa789cf8f90fe7bb2466dd0107642f
Andrii Vasyliev <sol@hiqdev.com>
2023-07-15 14:33:17 +0300

    read max backlight brightness from file

--- bin/backlight.go
+++ bin/backlight.go
@@ -10,9 +10,18 @@ func usage() {
     os.Exit(1)
 }
 
-const MIN_BRIGHTNESS = 1
-const MAX_BRIGHTNESS = 96000
 const CUR_BRIGHTNESS_PATH = "/sys/class/backlight/intel_backlight/brightness"
+const MAX_BRIGHTNESS_PATH = "/sys/class/backlight/intel_backlight/max_brightness"
+
+func ReadInt(path string) (int, error) {
+    bytes, err := os.ReadFile(path)
+    if err != nil {
+        return 0, err
+    }
+    text := strings.TrimSpace(string(bytes))
+    value, err := strconv.Atoi(text)
+    return value, err
+}
 
 func main() {
     if len(os.Args) < 2 {
@@ -28,28 +37,42 @@ func main() {
         usage()
     }
 
-    value, _ := strconv.ParseFloat(cmd[1:], 64)
+    max_value, err := ReadInt(MAX_BRIGHTNESS_PATH)
+    if max_value < 100 || err != nil {
+        fmt.Printf("Error reading max brightness(%d): %s\n", max_value, err)
+        os.Exit(1)
+    }
+
+    input := cmd[1:]
+    value, err := strconv.ParseFloat(input, 64)
+    if err != nil {
+        fmt.Printf("Error parsing value '%s': %s\n", input, err)
+        os.Exit(1)
+    }
 
     koef := 1.0 + sign * (value / 100)
     //fmt.Println(cmd, sign, value, koef)
 
-    bytes, _ := os.ReadFile(CUR_BRIGHTNESS_PATH)
-    text := strings.TrimSpace(string(bytes))
-    curr, _ := strconv.Atoi(text)
+    curr, err := ReadInt(CUR_BRIGHTNESS_PATH)
+    if err != nil {
+        fmt.Printf("Error reading brightness(%d): %s\n", curr, err)
+        os.Exit(1)
+    }
+
     next := int(float64(curr) * koef)
-    if next < MIN_BRIGHTNESS {
-        next = MIN_BRIGHTNESS
-    } else if next > MAX_BRIGHTNESS {
-        next = MAX_BRIGHTNESS
+    if next < 1 {
+        next = 1
+    } else if next > max_value {
+        next = max_value
     }
     //fmt.Println(text, curr, next)
 
     new_text := strconv.Itoa(next) + "\n"
 
-    err := os.WriteFile(CUR_BRIGHTNESS_PATH, []byte(new_text), 0644)
+    err = os.WriteFile(CUR_BRIGHTNESS_PATH, []byte(new_text), 0644)
     if err != nil {
-        fmt.Println(err)
+        fmt.Printf("Error writing brightness(%d): %s\n", next, err)
+    } else {
+        fmt.Printf("Brightness set to %d of %d, was %d\n", next, max_value, curr)
     }
-
-    fmt.Println(curr, next)
 }
