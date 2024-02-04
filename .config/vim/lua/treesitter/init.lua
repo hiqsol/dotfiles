82140e2526394671884b4e534d5ec28f9ae8213c
Andrii Vasyliev <sol@hiqdev.com>
2024-02-04 21:32:45 +0200

    vim: disable treesitter for git commit

--- .config/vim/lua/treesitter/init.lua
+++ .config/vim/lua/treesitter/init.lua
@@ -28,7 +28,7 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = { "markdown" },
+    disable = { "markdown", "gitcommit" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     --disable = function(lang, buf)
         --if lang == "markdown" then
