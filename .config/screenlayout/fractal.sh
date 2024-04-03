d845bbd3d1b59a27429423e7080ccdf10315bcd0
Andrii Vasyliev <sol@hiqdev.com>
2024-04-03 17:59:47 +0200

    add `fractal` screenlayout

---
+++ .config/screenlayout/fractal.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --mode 3840x2160 --pos 1200x0 --rotate right --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1200 --pos 0x1920 --rotate left --output DP-2 --off
