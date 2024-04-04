6cb27b47f11aedd46b36fca3ec51f9f99e1c9b94
Andrii Vasyliev <sol@hiqdev.com>
2024-04-04 10:47:26 +0200

    force 2560x1600 screen resolution at fractal

--- .config/screenlayout/fractal.sh
+++ .config/screenlayout/fractal.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 3840x2160 --pos 1200x0 --rotate right --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1200 --pos 0x1920 --rotate left --output DP-2 --off
+
+xrandr --newmode "2560x1600_30.00"  164.10  2560 2688 2960 3360  1600 1601 1604 1628  -HSync +Vsync
+xrandr --addmode HDMI-2 "2560x1600_30.00"
+
+xrandr --output DP-1 --mode 3840x2160 --pos 1600x0 --rotate right --output HDMI-1 --off --output HDMI-2 --primary --mode 2560x1600_30.00 --pos 0x0 --rotate left --output DP-2 --off
