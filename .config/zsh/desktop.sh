8859ecf26a7029d85cb95f3c35c33fc064b7414f
Andrii Vasyliev <sol@hiqdev.com>
2024-04-05 10:48:39 +0200

    add `kruler` - on screen ruler

--- .config/zsh/desktop.sh
+++ .config/zsh/desktop.sh
@@ -2,7 +2,7 @@
 install_desktop() {
     install_console
     sudo apt install konsole openvpn openssh-server \
-        keepassxc feh ffmpeg flameshot \
+        keepassxc feh ffmpeg flameshot kruler \
         arandr rhythmbox qt5-style-kvantum \
         fonts-terminus gnome-font-viewer fbreader
 
