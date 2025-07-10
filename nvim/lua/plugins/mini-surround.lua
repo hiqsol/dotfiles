e25def33011af024d36caea3f5cbc6060c60585b
Andrii <sol@solex.me>
2025-07-10 07:39:21 +0200

    vim: tune surround, disable flash

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -1,5 +1,18 @@
 return {
   "echasnovski/mini.surround",
+  version = false,
   event = "VeryLazy",
-  opts = {},
+  config = function()
+    require("mini.surround").setup({
+      mappings = {
+        add = "ys", -- keep Tip Pope style
+        delete = "ds",
+        replace = "cs",
+        find = "gsf", -- prefix with `g` to keep normal `s`
+        find_left = "gsF",
+        highlight = "gsh",
+        update_n_lines = "gsn",
+      },
+    })
+  end,
 }
