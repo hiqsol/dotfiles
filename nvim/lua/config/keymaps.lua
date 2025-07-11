a54f9c16dda351939fe254e36fccfbd5bcaf2dc3
Andrii <sol@solex.me>
2025-07-11 15:44:12 +0200

    vim: <C-S> save all files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-S>", ":wall<CR>", { desc = "Save all files" })
+vim.keymap.set("i", "<C-S>", "<Esc>:wall<CR>", { desc = "Save all files" })
