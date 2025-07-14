7531f85ace8370236c883cbff2c731cf48f57669
Andrii <sol@solex.me>
2025-07-14 08:41:56 +0200

    WIP Gp.nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,3 +5,12 @@
 vim.keymap.set("n", "<C-S>", ":wall<CR>", { desc = "Save all files" })
 vim.keymap.set("i", "<C-S>", "<Esc>:wall<CR>", { desc = "Save all files" })
 vim.keymap.set("n", "<C-X>", "ZZ", { desc = "Save and close" })
+
+vim.keymap.set({ "n", "i" }, "<C-g><C-r>", "<cmd>GpRewrite<cr>", { desc = "Inline Rewrite" })
+vim.keymap.set({ "n", "i" }, "<C-g><C-g>", "<cmd>GpAppend<cr>", { desc = "Append (after)" })
+vim.keymap.set({ "n", "i" }, "<C-g><C-b>", "<cmd>GpPrepend<cr>", { desc = "Prepend (before)" })
+
+vim.keymap.set("v", "<C-g><C-r>", ":<C-u>'<,'>GpRewrite<cr>", { desc = "Visual Rewrite" })
+vim.keymap.set("v", "<C-g><C-g>", ":<C-u>'<,'>GpAppend<cr>", { desc = "Visual Append (after)" })
+vim.keymap.set("v", "<C-g><C-b>", "<Cmd>'<,'>GpPrepend<CR>", { desc = "Visual Prepend (before)" })
+vim.keymap.set("v", "<C-g><C-t>", ":<C-u>'<,'>GpImplement<cr>", { desc = "Implement selection" })
--- nvim/lua/plugins/gp.lua
+++ nvim/lua/plugins/gp.lua
@@ -3,16 +3,8 @@ return {
   config = function()
     require("gp").setup({
       openai_api_key = os.getenv("OPENAI_API_KEY"),
-      agents = {
-        {
-          name = "GPT-4o",
-          model = "gpt-4o",
-          system_prompt = "Ти помічник для кодування та написання текстів українською та англійською.",
-          temperature = 0.7,
-        },
-      },
-      chat_agent = "GPT-4o",
-      command_agent = "GPT-4o",
+      chat_agent = "ChatGPT-4o",
+      command_agent = "ChatGPT-4o",
     })
   end,
 }
