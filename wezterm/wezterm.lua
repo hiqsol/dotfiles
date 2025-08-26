4c8a4633117de2cacc3e321e292f50f9973b5c6b
Andrii Vasyliev <sol@hiqdev.com>
2025-08-26 15:53:58 +0200

    wezterm: add Alt-N keys to switch tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -25,36 +25,23 @@ return {
 
 	-- Keybindings for splits
 	keys = {
-		{
-			key = "Enter",
-			mods = "ALT",
-			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "Enter",
-			mods = "CTRL",
-			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "h",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "l",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "k",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "j",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Down"),
-		},
+		{ key = "Enter", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "Enter", mods = "CTRL", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+		{ key = "h", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+
+		{ key = "1", mods = "ALT", action = wezterm.action.ActivateTab(0) },
+		{ key = "2", mods = "ALT", action = wezterm.action.ActivateTab(1) },
+		{ key = "3", mods = "ALT", action = wezterm.action.ActivateTab(2) },
+		{ key = "4", mods = "ALT", action = wezterm.action.ActivateTab(3) },
+		{ key = "5", mods = "ALT", action = wezterm.action.ActivateTab(4) },
+		{ key = "6", mods = "ALT", action = wezterm.action.ActivateTab(5) },
+		{ key = "7", mods = "ALT", action = wezterm.action.ActivateTab(6) },
+		{ key = "8", mods = "ALT", action = wezterm.action.ActivateTab(7) },
+		{ key = "9", mods = "ALT", action = wezterm.action.ActivateTab(8) },
 	},
 
 	-- Mouse-friendly copy/paste
