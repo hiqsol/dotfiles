b4cf1cdc3d268934a173e4e2243a91dc3b01a902
Andrii Vasyliev <sol@hiqdev.com>
2025-08-31 16:19:22 +0200

    vim: change default tabstop to 4

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.shiftwidth = 2 -- Number of spaces for auto-indent
-vim.opt.softtabstop = 2 -- Number of spaces when hitting TAB
-vim.opt.tabstop = 2 -- Number of visual spaces per TAB
+vim.opt.shiftwidth = 4 -- Number of spaces for auto-indent
+vim.opt.softtabstop = 4 -- Number of spaces when hitting TAB
+vim.opt.tabstop = 4 -- Number of visual spaces per TAB
 
 vim.opt.conceallevel = 0
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff" }) -- white
