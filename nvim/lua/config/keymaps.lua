042632e5964735f5eb339dcbdc12a48940606a07
Andrii Vasyliev <sol@hiqdev.com>
2025-09-16 22:20:07 +0200

    vim: tune git keymaps: gd, gB
    `gd` - git diff this file in vertical split
    `gB` - git blame this file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>gd", "<cmd>Gitsigns diffthis<CR>", { desc = "Git Diff (buffer)" })
+vim.keymap.set("n", "<leader>gB", "<cmd>Gitsigns blame<CR>", { desc = "Git Blame (buffer)" })
+vim.keymap.set("n", "<leader>gD", function()
+  Snacks.picker.git_diff()
+end, { desc = "Git Diff (hunks)" })
+
+vim.keymap.set({ "n", "x" }, "<leader>go", function()
+  Snacks.gitbrowse()
+end, { desc = "Git Browse (open)" })
+
 vim.keymap.set("n", "<C-S>", ":wall<CR>", { desc = "Save all files" })
 vim.keymap.set("i", "<C-S>", "<Esc>:wall<CR>", { desc = "Save all files" })
 vim.keymap.set("n", "<C-X>", "ZZ", { desc = "Save and close" })
+
 vim.keymap.set("n", "<leader>ip", function()
   vim.api.nvim_put({ vim.fn.expand("%:p") }, "l", true, true)
 end, { desc = "Insert full path below" })
