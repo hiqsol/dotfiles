c35295fa31d8cf952e78a405d78eb661c385be0a
Andrii Vasyliev <sol@hiqdev.com>
2025-09-21 19:36:59 +0200

    vim: remove `dressing` plugin

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -37,7 +37,6 @@ return {
     "nvim-mini/mini.pick", -- for file_selector provider mini.pick
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "stevearc/dressing.nvim", -- for input provider dressing
     "folke/snacks.nvim", -- for input provider snacks
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
