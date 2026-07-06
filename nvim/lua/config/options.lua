40a9d03c87b368fd555a0ca226eee5d1ed7c4bd5
Andrii Vasyliev <sol@hiqdev.com>
2026-07-06 06:50:36 +0000

    nvim: disable mouse

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,5 +3,6 @@ vim.opt.shiftwidth = 4 -- Number of spaces for auto-indent
 vim.opt.softtabstop = 4 -- Number of spaces when hitting TAB
 vim.opt.tabstop = 4 -- Number of visual spaces per TAB
 
+vim.opt.mouse = ""
 vim.opt.conceallevel = 0
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff" }) -- white
